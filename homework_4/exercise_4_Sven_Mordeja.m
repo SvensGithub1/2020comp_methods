77dd36c44363f3a3238a010ef60b8170840fcb1b
Sven Mordeja <smordeja@gmail.com>
2020-04-01 16:44:25 +0300

    commenting and plotting nicer

--- homework_4/exercise_4_Sven_Mordeja.m
+++ homework_4/exercise_4_Sven_Mordeja.m
@@ -1,69 +1,81 @@
-% Hands on solution for Simple Mechanism task
+% solution for simple slider-crank mechanism
 close all
 clear
-% u = [phi_2; d];
+% u = [theta; d];
 a = 0.1;
 b = 0.2;
 step_size=0.01;
 t=0:step_size:10;
-phi_1 = deg2rad(30) + t;
+phi = deg2rad(30) + t;
 u_ges =zeros(2,length(t));
 % set a reasonable starting point
 u0 = [0; b + a];
-eps = 1e-9;
-J = @(u) jacobian(u, b);
-
-for ii=1:length(t)
-% create function handles
-F = @(u) constraint(u, a, b, phi_1(ii));
+eps = 1e-9; %accuracy of computation
+J = @(u) jacobian(u, b); % create function handles
+for ii=1:length(t) % compute results for differnt time steps
+F = @(u) constraint(u, a, b, phi(ii));% create function handles
 
 [u, iteration_counter] = NR_method(F, J, u0, eps);
 u_ges(:,ii) = u;
 %fprintf('\n\tMechanism valid position is for d = %gm and phi1 = %gdeg\n\n', ...
  %   u(2), rad2deg(u(1)));
 end
-%[phi_2; d] = u;
+% u_ges(1,:) is theta calculated
+% u_ges(2,:) is d calculated
+%% plots
 figure(1)
 plot(t,u_ges(1,:),'linewidth', 2)
 xlabel('t [s]')
-ylabel('Theta [rad]')
-title('Theta over time')
-saveas(figure(1),'theta_over_time','emf')
+ylabel('\theta [rad]')
+title('\theta over time')
+
+figure_1_gca=gca;
+
 
 figure(2)
-plot(t,u_ges(2,:),'g','linewidth', 2)
+plot(t,u_ges(2,:),'r','linewidth', 2)
 xlabel('t [s]')
 ylabel('d [m]')
 title('d over time')
-saveas(figure(2),'d_over_time','emf')
+
+figure_2_gca=gca;
 
 dd_dt = diff(u_ges(2,:))/step_size;   % first derivative
 dTheta_dt = diff(u_ges(1,:))/step_size;   % second derivative
 
 figure(3)
-plot(t(1:length(dd_dt)),dd_dt,'r','linewidth', 2)
+plot(t(1:length(dd_dt)),dd_dt,'linewidth', 2)
 xlabel('t [s]')
 ylabel('v [m/s]')
 title('v over time')
-saveas(figure(3),'v_over_time','emf')
+
+figure_3_gca=gca;
 
 figure(4)
-plot(t(1:length(dTheta_dt)),dTheta_dt,'k','linewidth', 2)
+plot(t(1:length(dTheta_dt)),dTheta_dt,'r','linewidth', 2)
 xlabel('t [s]')
-ylabel('dTheta/dt [rad/s]')
-title('angular velocity dTheta/dt over time')
-saveas(figure(4),'angular_v_over_time','emf')
+ylabel('d\theta/dt [rad/s]')
+title('d\theta/dt over time')
+figure_4_gca=gca;
 
-function P = constraint(u, a, b, phi_1)
-phi_2 = u(1);
+set([figure_1_gca figure_2_gca figure_3_gca figure_4_gca],'LineWidth',1,'fontsize',18,'FontName','Times New Roman')
+
+
+saveas(figure(1),'theta_over_time','emf')
+saveas(figure(2),'d_over_time','emf')
+saveas(figure(3),'v_over_time','emf')
+saveas(figure(4),'angular_v_over_time','emf')
+%% constraons and jacobian matrix construction
+function P = constraint(u, a, b, phi)
+theta = u(1);
 d = u(2);
 
-P = [a * cos(phi_1) + b * cos(phi_2) - d
-    a * sin(phi_1) - b * sin(phi_2)];
+P = [a * cos(phi) + b * cos(theta) - d
+    a * sin(phi) - b * sin(theta)];
 end
 
 function P = jacobian(u, b)
-phi_2 = u(1);
-P = [-b * sin(phi_2), -1
-    -b * cos(phi_2), 0];
+theta = u(1);
+P = [-b * sin(theta), -1
+    -b * cos(theta), 0];
 end
